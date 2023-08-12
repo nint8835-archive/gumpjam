ded6b716b1825136f9d221d1531ae66ed04dd438
Riley Flynn <riley@rileyflynn.me>
2023-08-12 14:05:13 -0230

    Fix screen edge transitions

--- pkg/systems/screen_edge_transition.go
+++ pkg/systems/screen_edge_transition.go
@@ -8,26 +8,28 @@ import (
 
 type ScreenEdgeTransition struct {
 	*components.Position
+	*components.Hitbox
 }
 
 func (s *ScreenEdgeTransition) Update(w engine.World) {
 	screenWidth, screenHeight := float64(w.Bounds().Dx()), float64(w.Bounds().Dy())
 
-	if s.X < 0 {
+	if s.X+s.Hitbox.Width < 0 {
 		s.CellX--
-		s.X = screenWidth + s.X
+		s.X = screenWidth + s.X + s.Hitbox.Width - 0.01
 	}
 	if s.X > screenWidth {
 		s.CellX++
-		s.X = s.X - screenWidth
+		s.X = s.X - screenWidth - s.Hitbox.Width + 0.01
 	}
-	if s.Y < 0 {
+
+	if s.Y+s.Hitbox.Height < 0 {
 		s.CellY--
-		s.Y = screenHeight + s.Y
+		s.Y = screenHeight + s.Y + s.Hitbox.Height - 0.01
 	}
 	if s.Y > screenHeight {
 		s.CellY++
-		s.Y = s.Y - screenHeight
+		s.Y = s.Y - screenHeight - s.Hitbox.Height + 0.01
 	}
 }
 
