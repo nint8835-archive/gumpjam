f4671c2aea0c602295423dd7acce4cb727d991c4
Riley Flynn <riley@rileyflynn.me>
2023-08-13 17:29:44 -0230

    Symmetry

--- pkg/systems/debug.go
+++ pkg/systems/debug.go
@@ -36,13 +36,10 @@ func (d *Debug) Draw(w engine.World, screen *ebiten.Image) {
 	ebitenutil.DebugPrint(
 		screen,
 		fmt.Sprintf(
-			"CellX: %d, CellY: %d - %f FPS\nVX: %f, VY: %f, on ground: %t",
+			"CellX: %d, CellY: %d - %f FPS",
 			d.CellX,
 			d.CellY,
 			ebiten.ActualFPS(),
-			d.Velocity.X,
-			d.Velocity.Y,
-			d.Velocity.OnGround,
 		),
 	)
 }
